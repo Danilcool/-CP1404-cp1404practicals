80b1cb92f5f3928f87c5e5234f45ef8a58a6557a
Danilcool <danilovcharenk2@gmail.com>
2023-03-27 10:57:15 +0800

    Ubdate for password_checker_2

--- Prac_03/password_checker_2.py
+++ Prac_03/password_checker_2.py
@@ -3,11 +3,9 @@ CP1404/CP5632 - Practical
 Password checker "skeleton" code to help you get started
 """
 
-MIN_LENGTH = 5
-MAX_LENGTH = 15
+MIN_LENGTH = 2
+MAX_LENGTH = 6
 SPECIAL_CHARS_REQUIRED = False
-SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
-
 
 def main():
     """Program to get and check a user's password."""
@@ -17,8 +15,7 @@ def main():
     print("\t1 or more uppercase characters")
     print("\t1 or more lowercase characters")
     print("\t1 or more numbers")
-    if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+
     password = input("> ")
     while not is_valid_password(password):
         print("Invalid password!")
@@ -43,8 +40,6 @@ def is_valid_password(password):
             count_lower += 1
         elif char.upper():
             count_upper += 1
-        if char in SPECIAL_CHARACTERS:
-            count_special += 1
         # TODO: count each kind of character (use str methods like isdigit)
 
 
@@ -52,11 +47,6 @@ def is_valid_password(password):
     if count_lower == 0 or count_upper == 0 or count_digit == 0:
         return False
 
-    if SPECIAL_CHARS_REQUIRED:
-        if count_special == 0:
-            return False
-
-        return False
     # TODO: if special characters are required, then check the count of those
     # and return False if it's zero
 
