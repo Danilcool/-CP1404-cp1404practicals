56b5be62744b46c07462d23a786f70e1e18e2f5d
Danilcool <danilovcharenk2@gmail.com>
2023-05-22 20:57:45 +0800

    Prac_09

---
+++ Practical_10/Flask.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Temperature conversion function
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+# Route for Celsius to Fahrenheit conversion
+@app.route('/convert/<celsius>')
+def convert_celsius_to_fahrenheit(celsius):
+    try:
+        celsius = float(celsius)
+        fahrenheit = celsius_to_fahrenheit(celsius)
+        return f"The temperature of {celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit."
+    except ValueError:
+        return "Invalid input. Please enter a valid temperature in Celsius."
+
+if __name__ == '__main__':
+    app.run()
